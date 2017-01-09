7ded4d70a0a95d68fd7f9678b7fe4a5025b49b37
James <jbean46@gmail.com>
2017-01-09 07:17:00 +0100

    Inject Matrix for example

--- matrix_chain_order/matrix_chain_order_swift/matrix_chain_order_swift/main.swift
+++ matrix_chain_order/matrix_chain_order_swift/matrix_chain_order_swift/main.swift
@@ -8,29 +8,41 @@
 
 import Foundation
 
-func matrixChainOrder(dimensions: [Int]) -> (multiplications: [[Int]], splits: [[Int]]) {
-    let n = dimensions.count - 1
-    var multiplications = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
-    var splits = [[Int]](repeating: [Int](repeating: 0, count: n - 1), count: n - 1)
+func chainOrder(dimensions: [Int]) -> (Matrix<Int>, Matrix<Int>) {
     
-    // try every possible chain length
-    for chainLength in 2...n {
-        // try every possible left index given the chain length
-        for left in 0...n-chainLength {
-            // the right bound is inferred from the left index
+    let n = dimensions.count - 1
+    
+    var multiplications = Matrix(height: n, width: n, initial: 0)
+    var splits = Matrix(height: n - 1, width: n - 1, initial: 0)
+    
+    // Try every possible chain length
+    for chainLength in 2 ... n {
+        
+        // Try every possible left index given the chain length
+        for left in 0 ..< (n - chainLength) {
+            
+            // Infer right bound
             let right = left + chainLength - 1
-            // try to find the minimum number of multiplications
-            multiplications[left][right] = Int.max
-            // try every possible index for splitting the chain
-            for mid in left...right-1 {
-                // new possible minimum = number of multiplications needed to get to this point
-                var x = multiplications[left][mid] + multiplications[mid + 1][right]
-                // plus the number of multiplications needed to multiply the two resulting matrices
-                x += dimensions[left] * dimensions[mid + 1] * dimensions[right + 1]
-                // store a new minimum
-                if x < multiplications[left][right] {
-                    multiplications[left][right] = x
-                    splits[left][right - 1] = mid
+            
+            // Try to find the minimum number of multiplications
+            multiplications[left, right] = Int.max
+            
+            // Try every possible index for splitting the chain
+            for mid in left ..< right {
+                
+                // TODO: Consider making a helper function for this
+                // New possible minimum = number of multiplications needed to get to this point
+                // Plus the number of multiplications needed to multiply the two resulting 
+                // matrices
+                let x = (
+                    (multiplications[left, mid] + multiplications[mid + 1, right]) +
+                    (dimensions[left] * dimensions[mid + 1] * dimensions[right + 1])
+                )
+                
+                // Store a new minimum
+                if x < multiplications[left, right] {
+                    multiplications[left, right] = x
+                    splits[left, right - 1] = mid
                 }
             }
         }
@@ -39,50 +51,10 @@ func matrixChainOrder(dimensions: [Int]) -> (multiplications: [[Int]], splits: [
     return (multiplications, splits)
 }
 
-/// - note: Puts the given `separator` between elements of the nested array.
-func format <T> (_ array: [[T]], separator: String = "  ") -> String {
-    
-    /// Returns the width of a string-interpolated representation of any value.
-    ///
-    /// - warning: Assumes primitive type with no fancier `CustomStringConvertible` implementation.
-    func width(_ value: Any) -> Int {
-        return "\(value)".characters.count
-    }
-    
-    /// - returns: Whitespace with the given width.
-    func space(_ amount: Int) -> String {
-        return String(repeating: " ", count: amount)
-    }
-    
-    /// - returns: Maximum column width of 2D array.
-    ///
-    /// - note: Assumes length of each subarrays are equivalent
-    /// - note: Assumes `T` values don't have a complex `CustomStringConvertible` implementation.
-    func columnWidth <T> (_ array: [[T]]) -> Int {
-        
-        guard !array.isEmpty else {
-            return 0
-        }
-        
-        return (0 ..< array[0].count)
-            .flatMap { column in array.flatMap { row in width(row[column]) }.max() }
-            .max() ?? 0
-    }
-
-    /// - warning: Don't use `\t`, though. Doesn't register correctly.
-    func format <T> (_ array: [T], columnWidth: Int, separator: String = "  ") -> String {
-        return array.map { "\($0)\(separator)\(space(columnWidth - width($0)))" }.joined()
-    }
-    
-    return array
-        .map { format($0, columnWidth: columnWidth(array), separator: separator) }
-        .joined(separator: "\n")
-}
-
-let result = matrixChainOrder(dimensions:[30, 35, 15, 5, 10, 20, 25])
+let (multiplications, splits) = chainOrder(dimensions:[30, 35, 15, 5, 10, 20, 25])
 
 print("Multiplications:")
-print(format(result.multiplications))
+print(multiplications)
 print()
 print("Splits:")
-print(format(result.splits))
+print(splits)
