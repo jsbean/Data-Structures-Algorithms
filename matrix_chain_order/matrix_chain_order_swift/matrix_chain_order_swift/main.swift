67026948c9a4187406fe68bda589b39686d072e3
Brian Heim <brian@lovelocaldesign.com>
2017-01-08 19:08:12 -0500

    Add gitignore

---
+++ matrix_chain_order/matrix_chain_order_swift/matrix_chain_order_swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  matrix_chain_order_swift
+//
+//  Created by Brian Heim on 1/8/17.
+//  Copyright © 2017 Brian Heim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
